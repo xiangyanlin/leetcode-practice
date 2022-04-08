ac3034877ed33097d78e997d22e90ba212a22d4a
xiangyanlin <lin790825>
2022-04-08 21:52:01 +0800

    动态规划-- 翻译数字和最长不重复子串

--- src/main/java/org/example/xyl/swordfingeroffer/DynamicProgramSolution.java
+++ src/main/java/org/example/xyl/swordfingeroffer/DynamicProgramSolution.java
@@ -1,5 +1,10 @@
 package org.example.xyl.swordfingeroffer;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 动态规划
  *
@@ -248,8 +253,52 @@ public class DynamicProgramSolution {
     }
 
 
+    /**
+     * 把数字翻译成字符串
+     */
+    public int translateNum(int num) {
+
+        return f(num);
+    }
+    public int f(int num){
+        if(num < 10) {
+            return 1;
+        }
+        if (num % 100 < 26 && num % 100 > 9) {
+            return f(num / 10) + f(num / 100);
+        } else {
+            return f(num / 10);
+        }
+    }
+
+    /**
+     * 最长不重复子串
+     */
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         DynamicProgramSolution d = new DynamicProgramSolution();
+        System.out.println(d.lengthOfLongestSubstring("peekew"));
         System.out.println(d.fib(5));
     }
 }
