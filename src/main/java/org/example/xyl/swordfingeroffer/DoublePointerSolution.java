84e4445081ca66837d5b53a9783be466bb4f525e
xiangyanlin <lin790825>
2022-04-23 17:56:17 +0800

    两个列表第一个公共节点 & 合并两个排序的列表

--- src/main/java/org/example/xyl/swordfingeroffer/DoublePointerSolution.java
+++ src/main/java/org/example/xyl/swordfingeroffer/DoublePointerSolution.java
@@ -28,25 +28,25 @@ public class DoublePointerSolution {
     }
 
     public ListNode deleteNode1(ListNode head, int val) {
-        if(head.val == val) return head.next;
+        if (head.val == val) return head.next;
         ListNode pre = head, cur = head.next;
-        while(cur != null && cur.val != val) {
+        while (cur != null && cur.val != val) {
             pre = cur;
             cur = cur.next;
         }
-        if(cur != null) pre.next = cur.next;
+        if (cur != null) pre.next = cur.next;
         return head;
     }
 
     public ListNode getKthFromEnd(ListNode head, int k) {
-        while (head !=null) {
+        while (head != null) {
             ListNode last = head;
             int p = k;
-            while (p != 0){
+            while (p != 0) {
                 last = last.next;
                 p--;
             }
-            if(last == null) {
+            if (last == null) {
                 return head;
             }
             head = head.next;
@@ -71,15 +71,62 @@ public class DoublePointerSolution {
         return node;
     }
 
+    /**
+     * 合并两个排序的列表
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+
+    }
+
+
+    /**
+     * 两个列表第一个公共节点
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+
+
 
     public static void main(String[] args) {
         DoublePointerSolution test = new DoublePointerSolution();
         ListNode head = new ListNode(1);
-        head.next  = new ListNode(2);
-        head.next.next  = new ListNode(3);
-        head.next.next.next  = new ListNode(4);
-        head.next .next.next.next = new ListNode(5);
-        test.getKthFromEnd(head, 2);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        //test.getKthFromEnd(head, 2);
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode listNode = test.mergeTwoLists(l1, l2);
+        System.out.println("1");
     }
 
 }
