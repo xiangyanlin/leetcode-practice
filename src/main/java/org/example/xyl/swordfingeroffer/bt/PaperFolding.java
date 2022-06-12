976461da62f112953893e42c418cd5f9a0cf4788
xiangyanlin <lin790825>
2022-06-12 14:08:29 +0800

    二叉树的递归套路

---
+++ src/main/java/org/example/xyl/swordfingeroffer/bt/PaperFolding.java
@@ -0,0 +1,29 @@
+package org.example.xyl.swordfingeroffer.bt;
+
+/**
+ * 折纸问题
+ * @author xiangyanlin
+ * @date 2022/06/11
+ */
+public class PaperFolding {
+
+	public static void printAllFolds(int N) {
+		printProcess(1, N, true);
+	}
+
+	// 递归过程，来到了某一个节点，
+	// i是节点的层数，N一共的层数，down == true  凹    down == false 凸
+	public static void printProcess(int i, int N, boolean down) {
+		if (i > N) {
+			return;
+		}
+		printProcess(i + 1, N, true);
+		System.out.println(down ? "凹 " : "凸 ");
+		printProcess(i + 1, N, false);
+	}
+
+	public static void main(String[] args) {
+		int N = 3;
+		printAllFolds(N);
+	}
+}
