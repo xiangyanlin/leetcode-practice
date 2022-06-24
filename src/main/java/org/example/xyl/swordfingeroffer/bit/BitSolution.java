a6dba8ca40f9ea94e9ee49b26469549417571aba
xiangyanlin <xiangyl@didihu.com.cn>
2022-06-24 10:46:18 +0800

    剑指 Offer 56 - II. 数组中数字出现的次数 II 位运算中等

--- src/main/java/org/example/xyl/swordfingeroffer/bit/BitSolution.java
+++ src/main/java/org/example/xyl/swordfingeroffer/bit/BitSolution.java
@@ -40,16 +40,25 @@ public class BitSolution {
 
     public int singleNumber(int[] nums) {
         int[] counts = new int[32];
+
+        //统计每个二进制位1出现的次数
         for(int num : nums) {
             for(int j = 0; j < 32; j++) {
+                // 更新第 j 位
                 counts[j] += num & 1;
+                // 第 j 位 --> 第 j + 1 位
                 num >>>= 1;
             }
         }
+
         int res = 0, m = 3;
         for(int i = 0; i < 32; i++) {
             res <<= 1;
-            res |= counts[31 - i] % m;
+            // 得到 只出现一次的数字 的第 (31 - i) 位
+            int rem = counts[31 - i] % m;
+
+            //用或操作把每一位连接起来
+            res |= rem;
         }
         return res;
     }
