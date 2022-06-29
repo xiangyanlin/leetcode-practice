577112e78ab88f96f480a11851b34bfa238a7171
xiangyanlin <lin790825>
2022-06-29 23:53:33 +0800

    图

---
+++ src/main/java/graph/BFS.java
@@ -0,0 +1,35 @@
+package graph;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 图的宽度优先遍历
+ * @author xiangyanlin
+ * @date 2022/6/29
+ */
+public class BFS {
+
+	// 从node出发，进行宽度优先遍历
+	public static void bfs(Node node) {
+		if (node == null) {
+			return;
+		}
+		Queue<Node> queue = new LinkedList<>();
+		HashSet<Node> set = new HashSet<>();
+		queue.add(node);
+		set.add(node);
+		while (!queue.isEmpty()) {
+			Node cur = queue.poll();
+			System.out.println(cur.value);
+			for (Node next : cur.nexts) {
+				if (!set.contains(next)) {
+					set.add(next);
+					queue.add(next);
+				}
+			}
+		}
+	}
+
+}
