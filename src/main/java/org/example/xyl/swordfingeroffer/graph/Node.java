93d887b13164d7c1afb35968eba1b8043779e0a1
xiangyanlin <lin790825>
2022-07-05 23:09:58 +0800

    迪杰斯特拉（Dijkstra）算法

--- src/main/java/org/example/xyl/swordfingeroffer/graph/Node.java
+++ src/main/java/org/example/xyl/swordfingeroffer/graph/Node.java
@@ -1,6 +1,7 @@
 package org.example.xyl.swordfingeroffer.graph;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Node {
 	public int value;
@@ -15,5 +16,22 @@ public class Node {
 		out = 0;
 		nexts = new ArrayList<>();
 		edges = new ArrayList<>();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Node)) {
+			return false;
+		}
+		Node node = (Node) o;
+		return value == node.value && in == node.in && out == node.out && Objects.equals(nexts, node.nexts) && Objects.equals(edges, node.edges);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value, in, out, nexts, edges);
 	}
 }
