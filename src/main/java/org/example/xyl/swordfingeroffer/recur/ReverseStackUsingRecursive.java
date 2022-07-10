c5efec103b71201ea213e044eee5f45013b901bb
xiangyanlin <lin790825>
2022-07-10 13:10:17 +0800

    逆序栈

---
+++ src/main/java/org/example/xyl/swordfingeroffer/recur/ReverseStackUsingRecursive.java
@@ -0,0 +1,47 @@
+package org.example.xyl.swordfingeroffer.recur;
+
+import java.util.Stack;
+
+
+/**
+ * 逆序栈
+ * @author xiangyanlin
+ * @date 2022/7/7
+ */
+public class ReverseStackUsingRecursive {
+
+	public static void reverse(Stack<Integer> stack) {
+		if (stack.isEmpty()) {
+			return;
+		}
+		int i = f(stack);
+		reverse(stack);
+		stack.push(i);
+	}
+
+	public static int f(Stack<Integer> stack) {
+		int result = stack.pop();
+		if (stack.isEmpty()) {
+			return result;
+		} else {
+			int last = f(stack);
+			stack.push(result);
+			return last;
+		}
+	}
+
+	public static void main(String[] args) {
+		Stack<Integer> test = new Stack<Integer>();
+		test.push(1);
+		test.push(2);
+		test.push(3);
+		test.push(4);
+		test.push(5);
+		reverse(test);
+		while (!test.isEmpty()) {
+			System.out.println(test.pop());
+		}
+
+	}
+
+}
