1d95ddd2a27ab59c6d2488736bb949d9dcf73ce9
xiangyanlin <xiangyl@didihu.com.cn>
2022-07-11 18:20:10 +0800

    剑指 Offer 20. 表示数值的字符串 剑指 Offer 67. 把字符串转换成整数

---
+++ src/main/java/org/example/xyl/swordfingeroffer/analog/MaxQueue.java
@@ -0,0 +1,23 @@
+package org.example.xyl.swordfingeroffer.analog;
+
+/**
+ * @author xiangyanlin
+ * @date 2022/7/11
+ */
+public class MaxQueue {
+    public MaxQueue() {
+
+    }
+
+    public int max_value() {
+        return  0;
+    }
+
+    public void push_back(int value) {
+
+    }
+
+    public int pop_front() {
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/xyl/swordfingeroffer/analog/QueueSolution.java
@@ -0,0 +1,13 @@
+package org.example.xyl.swordfingeroffer.analog;
+
+import java.util.Map;
+
+/**
+ * @author xiangyanlin
+ * @date 2022/7/11
+ */
+public class QueueSolution {
+
+
+
+}
