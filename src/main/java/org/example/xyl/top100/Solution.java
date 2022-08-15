c084a96211b056f44750661bafab155add59f52b
xiangyanlin <xiangyl@didihu.com.cn>
2022-08-15 11:26:53 +0800

    top100 -2.两数相加

--- src/main/java/org/example/xyl/top100/Solution.java
+++ src/main/java/org/example/xyl/top100/Solution.java
@@ -2,7 +2,11 @@ package org.example.xyl.top100;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author xiangyanlin
@@ -63,9 +67,100 @@ public class Solution {
     }
 
 
+    int next = 0;
+
+    /**
+     * 两数相加
+     *
+     * @param l1 非负的整数 逆序
+     * @param l2 非负的整数 逆序
+     * @return 两数之和
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //base case
+        if (l1 == null && l2 == null) {
+            if (next != 0) {
+                return new ListNode(next);
+            }
+            return null;
+        }
+
+        int bit = next;
+        next = 0;
+        if (l1 != null) {
+            bit += l1.val;
+        }
+        if (l2 != null) {
+            bit += l2.val;
+        }
+        //求个位数
+        int val = 0;
+        if (bit < 10) {
+            val = bit;
+        } else {
+            val = bit % 10;
+            next = bit / 10;
+        }
+        ListNode node = new ListNode(val);
+        ListNode next1 = l1 == null ? null : l1.next;
+        ListNode next2 = l2 == null ? null : l2.next;
+        node.next = addTwoNumbers(next1, next2);
+        return node;
+    }
+
+    /**
+     * 官方解答
+     */
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = solution.twoSum(new int[]{2, 7, 11, 5}, 9);
-        System.out.println(Arrays.toString(arr));
+//        int[] arr = solution.twoSum(new int[]{2, 7, 11, 5}, 9);
+//        System.out.println(Arrays.toString(arr));
+
+        ListNode l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+        l1.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+        l2.next.next = new ListNode(9);
+        l2.next.next.next = new ListNode(9);
+        ListNode node = solution.addTwoNumbers(l1, l2);
+        while (node!= null){
+            System.out.println(node.val);
+            node = node.next;
+        }
     }
 }
