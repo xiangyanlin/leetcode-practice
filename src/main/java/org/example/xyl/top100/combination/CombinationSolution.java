71120492009c11d326b065cdc6395bf07a10749f
xiangyanlin <xiangyl@didihu.com.cn>
2022-09-22 14:47:30 +0800

    top100-20 46.全排列

--- src/main/java/org/example/xyl/top100/combination/CombinationSolution.java
+++ src/main/java/org/example/xyl/top100/combination/CombinationSolution.java
@@ -1,7 +1,10 @@
 package org.example.xyl.top100.combination;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author xiangyanlin
@@ -49,11 +52,100 @@ public class CombinationSolution {
         }
     }
 
+    /**
+     * 全排列
+     *
+     * @param nums 1 <= nums.length <= 6
+     *             -10 <= nums[i] <= 10
+     *             nums 中的所有整数 互不相同
+     * @return 所有不从夫的排列
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> combine = new ArrayList<>();
+        if (nums.length == 1) {
+            combine.add(nums[0]);
+            res.add(combine);
+            return res;
+        }
+        process(nums, 0, res);
+        return res;
+    }
+
+
+    public  void process(int[] nums, int i, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            res.add(list);
+        }
+        for (int j = i; j < nums.length; j++) {
+            swap(nums, i, j);
+            process(nums, i + 1, res);
+            swap(nums, i, j);
+        }
+    }
+
+    public void process2(int[] nums, int i, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            res.add(list);
+        }
+        // visit[0 1 .. 19] -10 <= nums[i] <= 10
+        boolean[] visit = new boolean[21];
+        for (int j = i; j < nums.length; j++) {
+            if (!visit[nums[j] + 10]) {
+                visit[nums[j] + 10] = true;
+                swap(nums, i, j);
+                process2(nums, i + 1, res);
+                swap(nums, i, j);
+            }
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+
+
     public static void main(String[] args) {
         CombinationSolution solution = new CombinationSolution();
         int[] candidates = {2, 3, 6, 7};
-        List<List<Integer>> list = solution.combinationSum(candidates, 7);
-        System.out.println(list.toString());
+//        List<List<Integer>> list = solution.combinationSum(candidates, 7);
+//        System.out.println(list.toString());
+
+        int[] nums = {1,2,3};
+        List<List<Integer>> permute = solution.permute(nums);
+        System.out.println(permute.toString());
     }
 
 }
